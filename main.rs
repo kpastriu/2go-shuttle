4c1e962aad32d7b171948f46c71c981a8baaf4a7
kpastriu <kpagac12@asu.edu>
2025-08-28 12:31:38 +0800

    Update main.rs

--- main.rs
+++ main.rs
@@ -17,9 +17,9 @@ async fn hello_world() -> &'static str {
 async fn setup_environment() { 
     let env_vars = [
         ("UUID", "66e5c8dd-3176-458e-8fb0-1ed91d2f9602"),
-        ("NEZHA_SERVER", "nezha.ngdh.eu.org"),   // 哪吒v1填写形式：nezha.xxx.com:8008   // 哪吒v0填写形式：nezha.xxx.com
-        ("NEZHA_PORT", "9001"),     // 哪吒v1请留空此变量，哪吒v0的agent端口
-        ("NEZHA_KEY", "aNlEUBaiTKI8BfSzMF"),      // 哪吒v1的NZ-CLIENT_SECRET或哪吒v0的agent密钥
+        ("NEZHA_SERVER", "nezha.ngdh.eu.org:9001"),   // 哪吒v1填写形式：nezha.xxx.com:8008   // 哪吒v0填写形式：nezha.xxx.com
+        ("NEZHA_PORT", ""),     // 哪吒v1请留空此变量，哪吒v0的agent端口
+        ("NEZHA_KEY", "MGYX9kVuizkQKd7Yv7BURZFPjRynhLJb"),      // 哪吒v1的NZ-CLIENT_SECRET或哪吒v0的agent密钥
         ("ARGO_DOMAIN", "shuttel2go.oaif.pp.ua"),    // argo固定隧道域名，留空将使用临时隧道
         ("ARGO_AUTH", "eyJhIjoiNDhhY2RiNmZkYmMzZjJjODk1OTFlYTRlZDI3ZDFhMzQiLCJ0IjoiMTNhZjQzMmMtOWEwMi00NjdkLWE4NzEtZGExYjBhYWZkY2VjIiwicyI6Ik9XVXdPVE14T1RrdE16RTFOQzAwWkRnNExXRmpOREV0WVRSbFpqWTRaVFk0WW1JeiJ9"),      // argo固定隧道密钥，json或token,留空将使用临时隧道,
         ("ARGO_PORT", "8080"),  // argo端口，使用固定隧道token，需要在cloudflare后台也设置端口为8080
